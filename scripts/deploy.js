51d8f7d5a7c9a671c6f08db5c0465ca07d754508
Sohail Ahmed <82243670+12SohailBlockchain@users.noreply.github.com>
2023-07-17 16:48:45 +0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -22,3 +22,39 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+//for the mutiple smart contracts functions//
+
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account: " + deployer.address);
+
+  // Deploy First
+  const First = await ethers.getContractFactory("FirstContract");
+  const first = await First.deploy();
+
+  // Deploy Second
+  const Second = await ethers.getContractFactory("SecondContract");
+  const second = await Second.deploy();
+
+
+// Deploy Third
+const Third = await ethers.getContractFactory("ThirdContract");
+const third = await Third.deploy();
+
+
+  
+
+  console.log("Sohail: " + first.address);
+  console.log("Ahmed: " + second.address);
+  console.log("Laghari: " + third.address);
+}
+
+main()
+  .then(() => process.exit())
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
